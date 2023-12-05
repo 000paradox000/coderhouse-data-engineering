c72951ae1a757f194abdc45dcd80d980895b020e
('Ariel <ariel.calzada@gmail.com>
2023-12-05 18:50:33 -0500

    Add booklet/semana 1/ejercicio 1

---
+++ ejercicios/semana1/booklet/ejercicio_1/main.py
@@ -0,0 +1,25 @@
+"""
+Escribir un programa que lea un número impar por teclado. Si el usuario no
+introduce un número impar, debe repetirse el proceso hasta que lo introduzca
+correctamente.
+"""
+
+def main():
+    while True:
+        input_string = input("Digite un número impar: ")
+
+        if not input_string.isdigit():
+            print("\tNúmero inválido")
+            continue
+
+        n = int(input_string)
+
+        if n % 2 == 0:
+            print("\tNúmero inválido")
+            continue
+
+        print("\tNúmero válido")
+        break
+
+if __name__ == "__main__":
+    main()
---
+++ ejercicios/semana1/numero_impar.py
@@ -0,0 +1,12 @@
+def main():
+    while True:
+        try:
+            n = input("Introduce número impar: ")
+        except Exception as e:
+            print("Error")
+            continue
+
+
+
+if __name__ == "__main__":
+    main()
