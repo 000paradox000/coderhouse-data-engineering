eed87e1375737c148372f318178d507eb6012631
(Ariel <ariel.calzada@gmail.com>
2023-12-08 07:57:56 -0500

    Polish the query

--- ejercicios/semana1/booklet/sql/ejercicio_7/ejercicio.sql
+++ ejercicios/semana1/booklet/sql/ejercicio_7/ejercicio.sql
@@ -9,8 +9,8 @@
 
 SELECT
     agents.name AS AgentName,
-    CASE WHEN calls.productsold = 1 THEN AVG(calls.duration) ELSE '' END AS time_to_sell,
-    CASE WHEN calls.productsold = 0 THEN AVG(calls.duration) ELSE '' END AS time_lost
+    CASE WHEN calls.productsold = 1 THEN AVG(calls.duration) ELSE 0 END AS time_to_sell,
+    CASE WHEN calls.productsold = 0 THEN AVG(calls.duration) ELSE 0 END AS time_lost
 FROM
     calls,
     agents
@@ -22,3 +22,44 @@ GROUP BY
     agents.name
 ORDER BY
     AgentName ASC;
+
+-- ============================================================================
+
+SELECT
+    a.name,
+    SUM(
+        CASE
+            WHEN productsold = 0 THEN duration
+            ELSE 0
+        END
+    )
+    /
+    SUM(
+        CASE
+            WHEN productsold = 0 THEN 1
+            ELSE 0
+        END
+    ) AS avgWhenNotSold,
+    SUM(
+        CASE
+            WHEN productsold = 1 THEN duration
+            ELSE 0
+        END
+    )
+    /
+    SUM(
+        CASE
+            WHEN productsold = 1 THEN 1
+            ELSE 0
+        END
+    ) AS avgWhenSold
+    FROM
+        calls c
+    JOIN
+        agents a
+    ON
+        c.agentid = a.agentid
+    GROUP BY
+        a.name
+    ORDER BY
+        1;
