1bbd61e0edca6f817e7b840c418354029d88db7a
('Ariel <ariel.calzada@gmail.com>
2023-12-12 17:34:04 -0500

    Fix fastapi

--- exercises/personal/python_read_tables_with_fastapi_and_sqlalchemy/models/database.py
+++ exercises/personal/python_read_tables_with_fastapi_and_sqlalchemy/models/database.py
@@ -7,7 +7,8 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = os.environ["SQLALCHEMY_DATABASE_URL"]
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={},
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
