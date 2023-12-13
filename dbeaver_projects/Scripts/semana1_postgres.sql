e8982e590a6599ae1b4165076cbf2d2ae2778f16
(Ariel <ariel.calzada@gmail.com>
2023-12-13 09:52:21 -0500

    Review ejercicio 3 semana 1

--- dbeaver_projects/Scripts/semana1_postgres.sql
+++ dbeaver_projects/Scripts/semana1_postgres.sql
@@ -24,3 +24,16 @@ FROM
 WHERE
     UPPER(occupation) LIKE '%ENGINEER%'
 ORDER BY occupation ASC;
+
+-- ============================================================================
+-- Ejercicio 3
+-- Escribir una consulta que devuelva el ID del cliente, su nombre y una
+-- columna nueva llamada “Mayor30” que contenga "Sí" si el cliente tiene más
+-- de 30 años y "No" en caso contrario.
+
+SELECT
+    customerid,
+    name,
+    CASE WHEN age > 30 THEN 'SI' ELSE 'NO' END as mayor30
+FROM
+    customers;
--- exercises/semana1/sql/ejercicio_3/ejercicio.sql
+++ exercises/semana1/sql/ejercicio_3/ejercicio.sql
@@ -1,3 +1,5 @@
+-- ============================================================================
+-- Ejercicio 3
 -- Escribir una consulta que devuelva el ID del cliente, su nombre y una
 -- columna nueva llamada “Mayor30” que contenga "Sí" si el cliente tiene más
 -- de 30 años y "No" en caso contrario.
