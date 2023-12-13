bbca7626da520fe25f35cebc967b4ea11d4bd1c9
(Ariel <ariel.calzada@gmail.com>
2023-12-13 10:18:14 -0500

    Review ejercicio 7 semana 1

--- dbeaver_projects/Scripts/semana1_postgres.sql
+++ dbeaver_projects/Scripts/semana1_postgres.sql
@@ -117,3 +117,54 @@ GROUP BY
     agents.name
 ORDER BY
     agent_name ASC;
+
+-- ============================================================================
+-- Ejercicio 7
+-- Escribir una consulta que extraiga dos métricas del desempeño de los
+-- agentes de ventas que le interesan a su empresa:
+
+-- Para cada agente, cuántos segundos en promedio les toma vender un producto
+-- cuando tienen éxito.
+
+-- Para cada agente, cuántos segundos en promedio permanecen en el teléfono
+-- antes de darse por vencidos cuando no tienen éxito.
+
+SELECT
+    a.name,
+    SUM(
+        CASE
+            WHEN productsold = 0 THEN duration
+            ELSE 0
+        END
+    )
+    /
+    SUM(
+        CASE
+            WHEN productsold = 0 THEN 1
+            ELSE 0
+        END
+    ) AS avgWhenNotSold,
+    SUM(
+        CASE
+            WHEN productsold = 1 THEN duration
+            ELSE 0
+        END
+    )
+    /
+    SUM(
+        CASE
+            WHEN productsold = 1 THEN 1
+            ELSE 0
+        END
+    ) AS avgWhenSold
+    FROM
+        calls c
+    JOIN
+        agents a
+    ON
+        c.agentid = a.agentid
+    GROUP BY
+        a.name
+    ORDER BY
+        1;
+   
--- exercises/semana1/sql/ejercicio_7/solution.sql
+++ exercises/semana1/sql/ejercicio_7/solution.sql
@@ -1,3 +1,5 @@
+-- ============================================================================
+-- Ejercicio 7
 -- Escribir una consulta que extraiga dos métricas del desempeño de los
 -- agentes de ventas que le interesan a su empresa:
 
@@ -6,24 +8,6 @@
 
 -- Para cada agente, cuántos segundos en promedio permanecen en el teléfono
 -- antes de darse por vencidos cuando no tienen éxito.
-
-SELECT
-    agents.name AS AgentName,
-    CASE WHEN calls.productsold = 1 THEN AVG(calls.duration) ELSE 0 END AS time_to_sell,
-    CASE WHEN calls.productsold = 0 THEN AVG(calls.duration) ELSE 0 END AS time_lost
-FROM
-    calls,
-    agents
-WHERE
-    calls.agentid = agents.agentid
-    AND
-    calls.pickedup = 1
-GROUP BY
-    agents.name
-ORDER BY
-    AgentName ASC;
-
--- ============================================================================
 
 SELECT
     a.name,
