89800eef3faa5ef29af407e8174a3248da31ddaa
(Ariel <ariel.calzada@gmail.com>
2023-12-13 10:05:30 -0500

    Review ejercicio 4 semana 1

--- dbeaver_projects/Scripts/semana1_mssql.sql
+++ dbeaver_projects/Scripts/semana1_mssql.sql
@@ -37,3 +37,29 @@ SELECT
     CASE WHEN age > 30 THEN 'SI' ELSE 'NO' END as mayor30
 FROM
     customers;
+    
+-- ============================================================================
+-- Ejercicio 4
+-- Escribir una consulta que devuelva todas las
+-- llamadas realizadas a clientes de la profesión de ingeniería y muestre
+-- si son mayores o menores de 30, así como si terminaron comprando el
+-- producto de esa llamada.
+
+SELECT
+    calls.callid,
+	calls.customerid,
+    customers.name AS customer_name,
+    agents.name AS agent_name,
+    CASE WHEN customers.age > 30 THEN 'SI' ELSE 'NO' END AS mayor30,
+    CASE WHEN productsold = 1 THEN 'SI' ELSE 'NO' END AS compro
+FROM
+    calls,
+    customers,
+    agents
+WHERE
+    calls.customerid = customers.customerid
+    AND
+    agents.agentid = calls.agentid
+    AND
+    UPPER(customers.occupation) LIKE '%ENGINEER%';
+   
--- exercises/semana1/sql/ejercicio_4/ejercicio.sql
+++ exercises/semana1/sql/ejercicio_4/ejercicio.sql
@@ -1,3 +1,5 @@
+-- ============================================================================
+-- Ejercicio 4
 -- Escribir una consulta que devuelva todas las
 -- llamadas realizadas a clientes de la profesión de ingeniería y muestre
 -- si son mayores o menores de 30, así como si terminaron comprando el
@@ -5,10 +7,11 @@
 
 SELECT
     calls.callid,
-    agents.name,
-    customers.name,
-    CASE WHEN customers.age > 30 THEN 'SI' ELSE 'NO' END as mayor30,
-    CASE WHEN productsold = 1 THEN 'SI' ELSE 'NO' END as compro
+	calls.customerid,
+    customers.name AS customer_name,
+    agents.name AS agent_name,
+    CASE WHEN customers.age > 30 THEN 'SI' ELSE 'NO' END AS mayor30,
+    CASE WHEN productsold = 1 THEN 'SI' ELSE 'NO' END AS compro
 FROM
     calls,
     customers,
